d09fd43f63c91d43da53c226aa8b71d60ddb09bd
GabrielStahl <47419421+GabrielStahl@users.noreply.github.com>
2024-07-28 13:46:01 +0100

    Update train.py

--- train.py
+++ train.py
@@ -171,7 +171,7 @@ def main():
     
         # Save the model every 5 epochs
         if (epoch + 1) % 5 == 0 and (environment == 'local' or dist.get_rank() == 0):
-            save_path = f"{config.model_save_path}_{modality}_{modelID}_epoch_{epoch+1}.pth"
+            save_path = f"{config.model_save_path}{modality}_{modelID}_epoch_{epoch+1}.pth"
             if environment != 'local':
                 torch.save(model.module.state_dict(), save_path)
             else:
@@ -180,7 +180,7 @@ def main():
 
     # Save the trained model
     if environment == 'local' or dist.get_rank() == 0:
-        save_path = f"{config.model_save_path}_{modality}_{modelID}_final_epoch.pth"
+        save_path = f"{config.model_save_path}{modality}_{modelID}_final_epoch.pth"
         if environment != 'local':
             torch.save(model.module.state_dict(), save_path)
             print(f"Model saved to {save_path}")
